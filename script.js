28045d80d9c015b2ac97c9eda717fc1179bf3b63
Amin74-b <cinamoh153@gmail.com>
2025-07-02 14:01:33 +0200

    todolist

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const btn = do
+const btnDark = document.getElementById("dark");
+const taskInput = document.getElementById("input")
+const btnadd = document.getElementById("add")
+const btnsup = document.getElementById("supr")
+const timer = document.getElementById("time")
+const numTasks = document.getElementById("task")
+const itemsContainer = document.getElementById("box03")
+const body = document.body;
+
+//dark - light mode switch
+btnDark.addEventListener('click', darkmode)
+function darkmode() {
+    body.classList.toggle('light-mode');
+    body.classList.toggle('night');
+    btnDark.textContent = body.classList.contains('night') ? ' Night ' : 'light Mode';
+};
+
+//show or hide no hide message
